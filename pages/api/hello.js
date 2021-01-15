d91f7edb60d72a9f59b8a0baa580a68255381aaa
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-01-15 18:09:45 +0700

    checking env

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,11 @@
 import admin from '../../core/services/firebaseAdmin'
 
 export default async (req, res) => {
+  console.log(process.env.SERVICE_PRIVATE_KEY_ID)
+  console.log(process.env.SERVICE_PRIVATE_KEY)
+  console.log(process.env.SERVICE_CLIENT_EMAIL)
+  console.log(process.env.SERVICE_CLIENT_ID)
+
   const userToken = req.body.userToken
 
   const decodedToken = await admin.auth().verifyIdToken(userToken).catch(err => console.log("catch " + err))
