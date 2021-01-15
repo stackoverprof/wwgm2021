d755316e397d1c2dbb2e721f795f3d0d7e591803
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-01-15 17:04:55 +0700

    env fixing

--- core/services/firebaseAdmin.js
+++ core/services/firebaseAdmin.js
@@ -1,11 +1,7 @@
 import admin from 'firebase-admin'
 
-
-console.log(process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_KEY)
-console.log(process.env.NEXT_PUBLIC_SERVICE_PRIVATE_KEY)
-
-const accountKey = JSON.parse(process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_KEY)
-accountKey.private_key = process.env.NEXT_PUBLIC_SERVICE_PRIVATE_KEY
+const accountKey = JSON.parse(process.env.NEXT_PUBLIC_ACC_KEY)
+accountKey.private_key = process.env.NEXT_PUBLIC_PRIVATE
 
 const adminConfig = {
   credential: admin.credential.cert(accountKey)
