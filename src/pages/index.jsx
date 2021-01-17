edff097933a40aeb99e37ffc73a680932dcbcebf
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-01-18 00:24:27 +0700

    addind functions

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     return (
         <Wrapper>
             <h1><strong>AREKSGM</strong></h1>
-            <h2>Uji Coba Double Deployment</h2>
+            <h2>Uji Coba Adding Cloud Functions</h2>
             <div className="links">
                 {authState == 'guest' && <Link href="/login"><button>Login</button></Link>}
                 {authState == 'guest' && <Link href="/register"><button>Register</button></Link>}
