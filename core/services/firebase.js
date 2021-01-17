018db97e06dc620b0fa44dbcf2f2599d21b693e5
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-01-17 21:59:46 +0700

    hard expose client env

--- core/services/firebase.js
+++ core/services/firebase.js
@@ -6,11 +6,11 @@ import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyAydGicvm4iwHUgV8uW4sgoSvZG_T2f8sw",
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
+    authDomain: "areks-gm.firebaseapp.com",
+    projectId: "areks-gm",
+    storageBucket: "areks-gm.appspot.com",
+    messagingSenderId: "189477264449",
+    appId: "1:189477264449:web:fee71f0abce80489013ac0",
 }
 
 if (!firebase.apps.length) firebase.initializeApp(firebaseConfig)
