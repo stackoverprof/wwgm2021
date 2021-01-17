964717240273e02ec358d471d54c75fd0b2cbd44
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-01-18 01:06:48 +0700

    adding first api/halo functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.get("/halo", (req, res) => {
+  res.status(200).json({
+    message: "halo world",
+  });
+});
+
+exports.api = functions.region("asia-southeast2").https.onRequest(app);
