db7ab2cf3c32086bef0933efcc69a580f37cfa89
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-01-22 23:49:02 +0700

    add navbar

---
+++ components/molecules/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Styled from '@emotion/styled'
+
+const Navbar = () => {
+
+    return (
+        <Wrapper>
+            
+        </Wrapper>
+    )
+}
+
+const Wrapper = Styled.nav(() =>`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 88px;
+    z-index: 100;
+
+    background: #FFFFFF;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.25);
+`)
+
+export default Navbar
