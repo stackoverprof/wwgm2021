b178cc940ff8ebf5578d521d87afba37f38e2da8
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-01-28 00:35:40 +0700

    request by client success

--- pages/api/admin/manage/try-out/new.js
+++ pages/api/admin/manage/try-out/new.js
@@ -1,67 +1,91 @@
 import admin, { DB } from '../../../../../core/services/firebaseAdmin'
 import { v4 as uuid } from 'uuid'
 
-const Data = {
-    userToken: '4q84LlmAukYfb7iOsAlJvqbzTYm2',
-    title: 'Try Out Soshum 1',
-    cluster: 'soshum',
-    availability: {
-        status: 'limited',
-        start: 'January 28, 2021 at 10:00:00 AM UTC+7',
-        end: 'January 28, 2021 at 3:00:00 PM UTC+7'
-    },
-    durations: {
-        TPS1: 15,
-        TPS2: 15,
-        TPS3: 15,
-        TPS4: 15,
-        TPA1: 20,
-        TPA2: 20,
-        TPA3: 20,
-        TPA4: 20,
-    }
-}
+// sample of good request body
+// const Req = {
+//     userToken: '4q84LlmAukYfb7iOsAlJvqbzTYm2',
+//     title: 'Try Out Soshum 1',
+//     cluster: 'soshum',
+//     availability: {
+//         status: 'limited',
+//         start: 1611802800000,
+//         end: 1611820800000
+//     },
+//     durations: {
+//         TPS1: 15,
+//         TPS2: 15,
+//         TPS3: 15,
+//         TPS4: 15,
+//         TPA1: 20,
+//         TPA2: 20,
+//         TPA3: 20,
+//         TPA4: 20,
+//     }
+// }
 
 export default async (req, res) => {
-    const examId = `${Data.cluster}-${uuid()}`
+    if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({status: 'ERROR', message: `Bad Request: No request body`})
+    }
+    
+    const Req = req.body
+    const examId = `${Req.cluster}-${uuid()}`
+    let _dataInvalid = false
 
-    const ExamData = {
+    const validate = (data, type) => {
+        if (typeof data !== type || typeof data === 'undefined') {
+            _dataInvalid = true
+        }
+        return data
+    }
+
+    const validateTimeStamp = (data) => {
+        if (typeof data !== 'number') {
+            _dataInvalid = true
+            return
+        }
+        return admin.firestore.Timestamp.fromMillis(data)
+    }
+    
+    const ExamFormat = {
         examId: examId,
-        title: Data.title,
-        cluster: Data.cluster,
+        title: validate(Req.title, 'string'),
+        cluster: validate(Req.cluster, 'string'),
         availability: {
-            status: Data.availability.status,
-            start: Data.availability.start,
-            end: Data.availability.end
+            status: validate(Req.availability.status, 'string'),
+            start: validateTimeStamp(Req.availability.start),
+            end: validateTimeStamp(Req.availability.end)
         },
         durations: {
-            TPS1: Data.durations.TPS1,
-            TPS2: Data.durations.TPS2,
-            TPS3: Data.durations.TPS3,
-            TPS4: Data.durations.TPS4,
-            TPA1: Data.durations.TPA1,
-            TPA2: Data.durations.TPA2,
-            TPA3: Data.durations.TPA3,
-            TPA4: Data.durations.TPA4
+            TPS1: validate(Req.durations.TPS1, 'number'),
+            TPS2: validate(Req.durations.TPS2, 'number'),
+            TPS3: validate(Req.durations.TPS3, 'number'),
+            TPS4: validate(Req.durations.TPS4, 'number'),
+            TPA1: validate(Req.durations.TPA1, 'number'),
+            TPA2: validate(Req.durations.TPA2, 'number'),
+            TPA3: validate(Req.durations.TPA3, 'number'),
+            TPA4: validate(Req.durations.TPA4, 'number')
         },
         participans: [],
         security: [
             {
-                editor: Data.userToken,
+                editor: Req.userToken,
                 timestamp: admin.firestore.Timestamp.now()
             }
         ]
     }
 
-    await DB.collection("Exams").doc(examId).set(ExamData)
+    if (_dataInvalid) {
+        return res.status(400).json({status: 'ERROR', message: `Bad Request: Invalid Parametes`})
+    }
 
     const countSession = (i) => {
         const counter = Math.floor(((i-1)/15)+1)
         return i > 60 ? 'TPA' + (counter - 4) : 'TPS' + counter
     }
-    
-    for (let i = 1; i <= 120; i++) {
-        await DB.collection("Exams").doc(examId).collection('Questions').doc(`${('00' + i).slice(-3)}`).set({
+
+    const QuestionsFormat = (i) => { 
+        return {
             body: '',
             options: [
                 {
@@ -88,9 +112,23 @@ export default async (req, res) => {
             answer: '',
             explanation: '',
             session: countSession(i)
-        })
-    }    
+        }
+    }
 
+    //FIRESTORE DB INSERTION    
+    return DB.collection("Exams").doc(examId).set(ExamFormat).then(async () => {
+        let completelyStored = true
+        for (let i = 1; i <= 120; i++) {
+            await DB.collection("Exams").doc(examId)
+                .collection('Questions').doc(`${('00' + i).slice(-3)}`)
+                .set(QuestionsFormat(i))
+                .catch(() => completelyStored = false)
+        }
 
-    res.status(200).json({status: 'OK', message: `KODE TES ${examId}`})
+        console.log(`New Exam created : ${examId}`)
+
+        if (completelyStored) res.status(200).json({status: 'OK', message: `KODE TES ${examId}`})
+        else res.status(500).json({status: 'ERROR', message: `Not completely stored, try again`})
+    })
+    .catch(err => res.status(500).json({status: 'ERROR', message: `Firebase related err : ${err}`}))
 }
---
+++ pages/create-exam.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { css } from '@emotion/css'
+import Link from 'next/link'
+import to from '../core/routepath'
+import axios from 'axios'
+import { useAuth } from '../core/contexts/AuthContext'
+import UserOnlyRoute from '../core/routeblocks/UserOnlyRoute'
+
+import MainLayout from '../components/layouts/MainLayout'
+import Spinner from '../components/atomic/spinner/Circle'
+    
+const Dashboard = () => {
+    const { currentUser, role, authMethods } = useAuth()
+    const [data, setData] = useState('')
+
+    const CheckRole = async () => {
+        setData(null)
+        
+        axios.post('/api/admin/manage/try-out/new', {
+                userToken: await currentUser.getIdToken(),
+                title: 'Try Out Soshum 1',
+                cluster: 'soshum',
+                availability: {
+                    status: 'limited',
+                    start: 1611802800000,
+                    end: 1611820800000
+                },
+                durations: {
+                    TPS1: 15,
+                    TPS2: 15,
+                    TPS3: 15,
+                    TPS4: 15,
+                    TPA1: 20,
+                    TPA2: 20,
+                    TPA3: 20,
+                    TPA4: 20,
+                }
+            })
+        .then(res => setData(res.data.message))
+        .catch(err => setData(err.response.data.message))
+    }
+
+    return (
+        <UserOnlyRoute redirect={to.login}>
+            {currentUser && (
+                <MainLayout className={style}>
+                    <img src="" alt=""/>
+                    <p>Dashboard of {currentUser.displayName}</p>
+                    <div>
+                        <img src={currentUser.photoURL} alt=""/>
+                        <Link href={to.home}><button>BACK HOME</button></Link>
+                        <button onClick={CheckRole}>{data === null ? <Spinner /> : 'create exam'}</button>
+                        <button onClick={authMethods.handleSignout} className="red">LOGOUT</button>
+                    </div>
+                    <p>Admin Status : {role.admin ? 'admin' : 'false'}</p>
+                    <p>{data}</p>
+                </MainLayout>
+            )}
+        </UserOnlyRoute>
+    )
+}
+const style = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    
+    div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    p{
+
+        margin-top: 54px;
+        text-align: center;
+    }
+`
+    
+export default Dashboard
