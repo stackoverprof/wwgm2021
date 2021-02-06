b7ccf13d56b1aa78898fe96a6c26458cf51e3b54
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-02-06 16:12:08 +0700

    code splitting AuthArea

---
+++ components/molecular/AuthArea.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { css } from '@emotion/react'
+import Link from 'next/link'
+import { useAuth } from '@core/contexts/AuthContext'
+import to from '@core/routepath'
+
+const AuthArea = ({className, openAuthAction, setOpenAuthAction, showLogin}) => {
+    const { currentUser, role, authMethods, authState } = useAuth()
+
+    return (
+        <div css={style({openAuthAction})} className={className}>
+            {authState !== 'user' && <button onClick={showLogin}>LOGIN</button>}
+            {authState === 'user' && (
+                <div className="auth-area">
+                    <button onClick={() => setOpenAuthAction(!openAuthAction)} className="user-action btn flex-sc">
+                        <img src={currentUser.photoURL} alt=""/>
+                        <p>Angkasa</p>
+                    </button>
+                    <div className="auth-dropper flex-cc col">
+                        <Link href={to.dashboard}>DASHBOARD</Link>
+                        {role.admin && <Link href={to.dashboard}>ADMIN AREA</Link>}
+                        <button onClick={authMethods.handleSignout} className="btn red">LOG OUT</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+const style = ({openAuthAction}) => css`
+
+    .auth-area{
+        position: relative;
+        height: 46px;
+        max-width: 200px;
+    }
+
+    .auth-dropper{
+        position: absolute;
+        top: 114%;
+        width: 100%;
+        max-height: ${openAuthAction ? '100vh' : 0};
+        background: white;
+        box-shadow: 0 0 4px 0 #0005;
+        border-radius: 8px;
+        padding: ${openAuthAction ? '6px 0' : 0};
+        opacity: ${openAuthAction ? 1 : 0};
+        overflow: hidden;
+        transition: all 0.5s, opacity  ${openAuthAction ? '0.5s 0s' : '0.25s 0.25s'};
+
+        a{
+            color: #0F1A12;
+            margin: 6px 0;
+            transition: 0.1s;
+
+            &:hover{
+                transform: scale(1.02);
+            }
+        }
+        
+        button{
+            margin: 6px 0;
+            padding: 8px 10px !important;
+            transition: 0.5s ${openAuthAction ? '0s' : '.5s'};
+            width: 76%;
+            transform: scale(${openAuthAction ? 1 : 1.1});
+            font-size: 20px;
+        }
+    }
+
+    button.user-action{
+        padding: 0 !important;
+        height: 100%;
+        min-width: 160px;
+
+        p{
+            font-size: 16px;
+            font-weight: 600;
+        }
+        
+        img{
+            height: 30px;
+            width: 30px;
+            margin-right: 12px;
+            margin-left: 12px;
+            border-radius: 50%;
+        }
+    }
+
+`
+
+export default AuthArea
--- components/molecular/Navbar/index.jsx
+++ components/molecular/Navbar/index.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import { css } from '@emotion/react'
 import Link from 'next/link'
 import OutsideClickHandler from 'react-outside-click-handler'
-import { useAuth } from '@core/contexts/AuthContext'
 import to from '@core/routepath'
 
 import MenuButton from '@components/atomic/MenuButton'
 import LoginPopUp from '@components/molecular/LoginPopUp'
+import AuthArea from '@components/molecular/AuthArea'
 
 const Navbar = () => {
     const [openDropper, setOpenDropper] = useState(false)
@@ -25,7 +25,7 @@ const Navbar = () => {
 
     return (
     <>  
-        <nav css={style({openDropper, openAuthAction})}>
+        <nav css={style({openDropper})}>
             <OutsideClickHandler onOutsideClick={() => toggleDropper(false)} disabled={!openDropper && !openAuthAction}>
                 <div className="navbar-main flex-cc">
                     <div className="contain-size-xl flex-bc inner">
@@ -54,7 +54,6 @@ const Navbar = () => {
 }
 
 const LinkSet = ({openAuthAction, setOpenAuthAction, showLogin}) => {
-    const { currentUser, role, authMethods, authState } = useAuth()
     
     return (
     <>
@@ -76,27 +75,17 @@ const LinkSet = ({openAuthAction, setOpenAuthAction, showLogin}) => {
                 <div className="bar"></div>
             </div>
         </Link>
-        <div className="link-item login flex-cc">
-        {authState !== 'user' && <button onClick={showLogin}>LOGIN</button>}
-        {authState === 'user' && (
-            <div className="auth-area">
-                    <button onClick={() => setOpenAuthAction(!openAuthAction)} className="user-action btn flex-sc">
-                        <img src={currentUser.photoURL} alt=""/>
-                        <p>Angkasa</p>
-                    </button>
-                    <div className="auth-dropper flex-cc col">
-                        <Link href={to.dashboard}>DASHBOARD</Link>
-                        {role.admin && <Link href={to.dashboard}>ADMIN AREA</Link>}
-                        <button onClick={authMethods.handleSignout} className="btn red">LOG OUT</button>
-                    </div>
-                </div>
-        )}
-        </div>
+        <AuthArea 
+            className="link-item login flex-cc" 
+            openAuthAction={openAuthAction}
+            setOpenAuthAction={setOpenAuthAction}
+            showLogin={showLogin}
+        />
     </>
     )
 }
 
-const style = ({openDropper, openAuthAction}) => css` //Nav tag core style is in globals.scss
+const style = ({openDropper}) => css` //Nav tag core style is in globals.scss
     width: 100%;
     height: 64px;
     z-index: 100;
@@ -114,64 +103,6 @@ const style = ({openDropper, openAuthAction}) => css` //Nav tag core style is in
         background: rgba(255, 255, 255, ${openDropper ? 1 : 0.85});
         backdrop-filter: blur(8px);
         box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.25);
-    }
-
-    .auth-area{
-        position: relative;
-        height: 46px;
-        max-width: 200px;
-    }
-
-    .auth-dropper{
-        position: absolute;
-        top: 114%;
-        width: 100%;
-        max-height: ${openAuthAction ? '100vh' : 0};
-        background: white;
-        box-shadow: 0 0 4px 0 #0005;
-        border-radius: 8px;
-        padding: ${openAuthAction ? '6px 0' : 0};
-        opacity: ${openAuthAction ? 1 : 0};
-        overflow: hidden;
-        transition: all 0.5s, opacity  ${openAuthAction ? '0.5s 0s' : '0.25s 0.25s'};
-
-        a{
-            color: #0F1A12;
-            margin: 6px 0;
-            transition: 0.1s;
-
-            &:hover{
-                transform: scale(1.02);
-            }
-        }
-        
-        button{
-            margin: 6px 0;
-            padding: 8px 10px !important;
-            transition: 0.5s ${openAuthAction ? '0s' : '.5s'};
-            width: 76%;
-            transform: scale(${openAuthAction ? 1 : 1.1});
-            font-size: 20px;
-        }
-    }
-
-    button.user-action{
-        padding: 0 !important;
-        height: 100%;
-        min-width: 160px;
-
-        p{
-            font-size: 16px;
-            font-weight: 600;
-        }
-        
-        img{
-            height: 30px;
-            width: 30px;
-            margin-right: 12px;
-            margin-left: 12px;
-            border-radius: 50%;
-        }
     }
 
     .dropper{
@@ -271,8 +202,6 @@ const style = ({openDropper, openAuthAction}) => css` //Nav tag core style is in
         }
 
         &:hover {
-            transform: scale(1.02);
-
             .bar{
                 width: 90%;
                 opacity: 1;
@@ -284,6 +213,10 @@ const style = ({openDropper, openAuthAction}) => css` //Nav tag core style is in
 
             .bar{
                 display: none;
+            }
+
+            &:hover a{    
+                transform: scale(1.02);
             }
         }
     }
