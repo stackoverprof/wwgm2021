438f5113854aa5ca86a2c20bf7889b4e6fbbd4f8
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-02-23 11:39:30 +0700

    basic input

---
+++ components/atomic/PreviewSoal.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { css } from '@emotion/react'
+
+const PreviewSoal = () => {
+
+    return (
+        <div css={style}>
+            
+        </div>
+    )
+}
+
+const style = css`
+    
+`
+
+export default PreviewSoal
--- pages/area-admin/manage-exams/[examId]/edit.jsx
+++ pages/area-admin/manage-exams/[examId]/edit.jsx
@@ -6,16 +6,36 @@ import AdminOnlyRoute from '@core/routeblocks/AdminOnlyRoute'
 import { useAuth } from '@core/contexts/AuthContext'
 import FireFetcher from '@core/services/FireFetcher'
 import AdminLayout from '@components/layouts/AdminLayout'
+import axios from 'axios'
 
 const Edit = () => {
     const [questions, setQuestions] = useState([])
     const [answers, setAnswers] = useState([])
     const [activeIndex, setActiveIndex] = useState(0)
+    const [examData, setExamData] = useState(null)
+    const [inputData, setInputData] = useState({})
+    
     const { authState, access } = useAuth()
     const { query: { examId } } = useRouter()
 
+    const mutateInputData = ({target: { name, value }}) => {
+        setInputData((prevState) => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    const fetchData = async () => {
+        await axios.post('/api/public/exams/get-exam-data', {
+            examId: examId
+        })
+        .then(res => setExamData(res.data.body))
+        .catch(err => setGlobalAlert({error: true, body: err.response.data.message}))
+    }
+
     useEffect(() => {
         if (examId) {
+            fetchData()
             FireFetcher.listen.examQuestions(examId, {
                 attach: doc => {
                     setQuestions(doc.data().list)
@@ -35,29 +55,55 @@ const Edit = () => {
         }
     }, [examId])
 
-    useEffect(() => {
-        console.log(questions)
-    }, [questions])
-    
-    useEffect(() => {
-        console.log(answers)
-    }, [answers])
+    useEffect(() => { 
+        if (questions.length !== 0 && answers.length !== 0){
+            setInputData({
+                body: questions[activeIndex].body,
+                optionA: questions[activeIndex].options[0].body,
+                optionB: questions[activeIndex].options[1].body,
+                optionC: questions[activeIndex].options[2].body,
+                optionD: questions[activeIndex].options[3].body,
+                optionE: questions[activeIndex].options[4].body
+            })
+        }
+    }, [questions, answers, activeIndex])
 
     return (
         <AdminOnlyRoute>
             { authState === 'user' && access.admin && (
                 <AdminLayout css={style.page} title="Exam Control" className="flex-sc col">
                     <select value={activeIndex} onChange={e => setActiveIndex(e.target.value)} name="index-pad" id="index-pad">    
-                        {questions.map((item, i) => (
+                   
+                         {questions.map((item, i) => (
                             <option value={item.id - 1} key={i}>{item.id}</option>
                         ))}
                     </select>
-                    {questions.length !== 0 && answers.length !== 0 &&
-                        <>
-                            <h1>{questions[activeIndex].id}</h1>
-                            <h2>{answers[activeIndex].id}</h2>
-                        </>
-                    }
+                    <section css={style.form}>
+                        <div className="inner contain-size-s">
+                            {questions.length !== 0 && answers.length !== 0 &&
+                                <form className="flex-cc col">
+                                    <div className="input-group">
+                                        <textarea value={inputData.body} onChange={mutateInputData} name="body" id="body"></textarea>
+                                    </div>
+                                    <div className="input-group">
+                                        <textarea value={inputData.optionA} onChange={mutateInputData} name="optionA" id="optionA"></textarea>
+                                    </div>
+                                    <div className="input-group">
+                                        <textarea value={inputData.optionB} onChange={mutateInputData} name="optionB" id="optionB"></textarea>
+                                    </div>
+                                    <div className="input-group">
+                                        <textarea value={inputData.optionC} onChange={mutateInputData} name="optionC" id="optionC"></textarea>
+                                    </div>
+                                    <div className="input-group">
+                                        <textarea value={inputData.optionD} onChange={mutateInputData} name="optionD" id="optionD"></textarea>
+                                    </div>
+                                    <div className="input-group">
+                                        <textarea value={inputData.optionE} onChange={mutateInputData} name="optionE" id="optionE"></textarea>
+                                    </div>
+                                </form>
+                            }
+                        </div>
+                    </section>
                 </AdminLayout>
             )}
         </AdminOnlyRoute>
@@ -88,8 +134,21 @@ const style = {
             }
         }
     `,
-    userList: css`
-    
+    form: css`
+
+        form {
+            width: 100%;
+
+            .input-group {
+                width: 100%;
+            }
+            textarea {
+                width: 100%;
+                height: 100px;
+                min-height: 100px;
+                resize: vertical;
+            }
+        }
     `
 }
 export default Edit
