7aa377a379e4e0538a619d72191c30c799550141
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-03-01 19:33:53 +0700

    split code IntroBox

---
+++ components/molecular/Home/IntroBox.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import { css } from '@emotion/react'
+import { FaInstagram } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { RiLineFill } from 'react-icons/ri'
+
+const IntroBox = () => {
+
+    return (
+        <div css={style} className="content-container full-w">
+            <div className="upper full-w flex-cc">
+                <div className="inner contain-size-l flex-cc">
+                    <p>Wara-Wiri Gadjah Mada (WWGM) merupakan acara yang bertujuan untuk mengenalkan UGM kepada pelajar di Surabaya yang berbentuk TryOut berbasis utul dan faculty fair.</p>
+                    <img src="/img/illus/intro.svg" alt=""/>
+                </div>
+            </div>
+            <div className="lower flex-cc">
+                <div className="inner contain-size-l flex-sc">
+                    <p className="social flex-cc"><FaInstagram /> @areksgm</p>
+                    <p className="social flex-cc"><HiOutlineMail /> surabayagadjahmada@gmail.com</p>
+                    <p className="social flex-cc"><RiLineFill /> @nts6607c</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const style = css`
+    margin-top: 32px;
+    
+    .upper {
+        background: #1A2C1E;
+
+        @media (max-width: 760px) {
+            .inner {
+                flex-direction: column;
+                width: 70%;
+            }
+        }
+        
+        p {
+            font-family: Poppins;
+            font-weight: 600;
+            font-size: 20px;
+            margin-right: 24px;
+            color: #FFFFFF;
+            
+            @media (max-width: 760px) {
+                font-size: 16px;
+                margin: 54px 0 24px 0;
+                text-align: center;
+            }
+        }
+        
+        img {
+            position: relative;
+            height: 280px;
+            bottom: -24px;
+            object-position: bottom;
+            object-fit: contain;
+        }
+    }
+    
+    .lower {
+        background: #0F1A12;
+        padding: 24px 0;
+        
+        @media (max-width: 760px) {
+            padding: 32px 0;
+            
+            .inner {
+                flex-direction: column;
+            }
+        }
+        
+        p.social {
+            font-family: Poppins;
+            font-weight: 500;
+            font-size: 16px;
+            margin: 6px;
+            color: #FBC343;
+
+            svg {
+                margin-right: 6px;
+                font-size: 24px;
+            }
+        }
+    }
+`
+
+export default IntroBox
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { css } from '@emotion/react'
 import axios from 'axios'
-import { FaInstagram } from 'react-icons/fa'
-import { HiOutlineMail } from 'react-icons/hi'
-import { RiLineFill } from 'react-icons/ri'
 
 import { useLayout } from '@core/contexts/LayoutContext'
 import MainLayout from '@components/layouts/MainLayout'
 import LoginPopUp from '@components/molecular/PopUps/LoginPopUp'
 import Advantages from '@components/atomic/Advantages'
 import CardDisplay from '@components/atomic/CardDisplay'
+import IntroBox from '@components/molecular/Home/IntroBox'
 
 const Home = () => {
     const [openLoginPop, setOpenLoginPop] = useState(false)
@@ -68,21 +66,7 @@ const Home = () => {
                         <h2>Apa Sih WWGM 2021?</h2>
                     </div>
                     <hr className="head-divider full-w"/>
-                    <div className="content-container full-w">
-                        <div className="upper full-w flex-cc">
-                            <div className="inner contain-size-l flex-cc">
-                                <p>Wara-Wiri Gadjah Mada (WWGM) merupakan acara yang bertujuan untuk mengenalkan UGM kepada pelajar di Surabaya yang berbentuk TryOut berbasis utul dan faculty fair.</p>
-                                <img src="/img/illus/intro.svg" alt=""/>
-                            </div>
-                        </div>
-                        <div className="lower flex-cc">
-                            <div className="inner contain-size-l flex-sc">
-                                <p className="social flex-cc"><FaInstagram /> @areksgm</p>
-                                <p className="social flex-cc"><HiOutlineMail /> surabayagadjahmada@gmail.com</p>
-                                <p className="social flex-cc"><RiLineFill /> @nts6607c</p>
-                            </div>
-                        </div>
-                    </div>
+                    <IntroBox />
                 </div>
             </section>
 
@@ -123,67 +107,6 @@ const style = {
             position: relative;
             top: -30px;
             z-index: -1;
-        }
-
-        .content-container {
-            margin-top: 32px;
-        }
-        
-        .upper {
-            background: #1A2C1E;
-
-            @media (max-width: 760px) {
-                .inner {
-                    flex-direction: column;
-                    width: 70%;
-                }
-            }
-            
-            p {
-                font-family: Poppins;
-                font-weight: 600;
-                font-size: 20px;
-                margin-right: 24px;
-                color: #FFFFFF;
-                
-                @media (max-width: 760px) {
-                    font-size: 16px;
-                    margin: 48px 0;
-                    text-align: center;
-                }
-            }
-            
-            img {
-                position: relative;
-                height: 280px;
-                bottom: -24px;
-            }
-        }
-        
-        .lower {
-            background: #0F1A12;
-            padding: 24px 0;
-            
-            @media (max-width: 760px) {
-                padding: 32px 0;
-                
-                .inner {
-                    flex-direction: column;
-                }
-            }
-            
-            p.social {
-                font-family: Poppins;
-                font-weight: 500;
-                font-size: 16px;
-                margin: 6px;
-                color: #FBC343;
-
-                svg {
-                    margin-right: 6px;
-                    font-size: 24px;
-                }
-            }
         }
     `,
 
