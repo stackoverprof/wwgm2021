c832cbd46e38cd4ee49c5234551736dd03c33ed3
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-03-04 15:21:47 +0700

    responsivity fix

--- components/molecular/Home/Gallery.jsx
+++ components/molecular/Home/Gallery.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import { css } from '@emotion/react'
 
+const images = {
+    tryOut: [
+        'https://firebasestorage.googleapis.com/v0/b/areks-gm.appspot.com/o/Documentation%2FIMG_6886-min.webp?alt=media&token=52b26576-c4ce-4f91-9e90-bf3279d68f8d',
+        'https://firebasestorage.googleapis.com/v0/b/areks-gm.appspot.com/o/Documentation%2FIMG_6814-min.webp?alt=media&token=53f9ddb5-ac65-4535-92d3-0ba86dc1c544',
+        'https://firebasestorage.googleapis.com/v0/b/areks-gm.appspot.com/o/Documentation%2FIMG_6869-min.webp?alt=media&token=8d23b12d-f593-460e-b20a-868b86d80e54',
+        'https://firebasestorage.googleapis.com/v0/b/areks-gm.appspot.com/o/Documentation%2FIMG_6891-min.webp?alt=media&token=1f40ef0c-740c-4a15-a246-82d970004f4e',
+        'https://firebasestorage.googleapis.com/v0/b/areks-gm.appspot.com/o/Documentation%2FIMG_6849-min.webp?alt=media&token=b6c9a4e9-d0a3-4fa1-b55e-bfb2e8e1ce39',
+        'https://firebasestorage.googleapis.com/v0/b/areks-gm.appspot.com/o/Documentation%2FIMG_6824-min.webp?alt=media&token=ae359284-4cbb-4baf-8c7a-8e2ec40b20ae'
+    ],
+    facultyFair: [
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+    ]
+}
+
 const Gallery = () => {
 
     return (
-        <div css={style} className="full-w flex-cc">
-            <h3></h3>
+        <div css={style} className="full-w flex-cc col">
             <div className="contain-size-m">
+                <h3 className="full-w">Try Out</h3>
                 <div className="gallery full-w">
-                    <img className="gallery-image" src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=500&h=500&fit=crop" alt="person writing in a notebook beside by an iPad, laptop, printed photos, spectacles, and a cup of coffee on a saucer"/>
-                    <img className="gallery-image" src="https://images.unsplash.com/photo-1515260268569-9271009adfdb?w=500&h=500&fit=crop" alt="sunset behind San Francisco city skyline"/>
-                    <img className="gallery-image" src="https://images.unsplash.com/photo-1506045412240-22980140a405?w=500&h=500&fit=crop" alt="people holding umbrellas on a busy street at night lit by street lights and illuminated signs in Tokyo, Japan"/>
-                    <img className="gallery-image" src="https://images.unsplash.com/photo-1514041181368-bca62cceffcd?w=500&h=500&fit=crop" alt="car interior from central back seat position showing driver and blurred view through windscreen of a busy road at night"/>
-                    <img className="gallery-image" src="https://images.unsplash.com/photo-1445810694374-0a94739e4a03?w=500&h=500&fit=crop" alt="back view of woman wearing a backpack and beanie waiting to cross the road on a busy street at night in New York City, USA"/>
-                    <img className="gallery-image" src="https://images.unsplash.com/photo-1486334803289-1623f249dd1e?w=500&h=500&fit=crop" alt="man wearing a black jacket, white shirt, blue jeans, and brown boots, playing a white electric guitar while sitting on an amp"/>
+                    {images['tryOut'].map((image, i) => (
+                        <img src={image} alt="Dokumentasi wwgm tahun yang lalu" key={i}/>
+                    ))}
                 </div>
             </div>
         </div>
@@ -21,6 +37,19 @@ const Gallery = () => {
 }
 
 const style = css`
+    margin-top: 32px;
+
+    h3 {
+        color: var(--army);
+        font-weight: 700;
+        font-size: 54px;
+
+        @media (max-width: 572px) {
+            text-align: center;
+            font-size: 42px;
+        }
+    }
+
     .gallery {
         display: grid;
         grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
